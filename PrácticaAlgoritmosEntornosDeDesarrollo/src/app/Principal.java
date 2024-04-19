ab6a49d300a8788a6e10463ce120f31973573e34
agl0041 <agl0041@alu.medac.es>
2024-04-19 19:47:03 +0200

    añadiendo factoriales al main

--- PrácticaAlgoritmosEntornosDeDesarrollo/src/app/Principal.java
+++ PrácticaAlgoritmosEntornosDeDesarrollo/src/app/Principal.java
@@ -1,7 +1,8 @@
 //ALVARO GARCIA LAMUDA
 package app;
+import util.Algoritmos;
 
-public class Principal 
+public class Principal extends Algoritmos
 {
 	public static void main(String[] args) 
 	{
@@ -10,19 +11,28 @@ public class Principal
 		int []resultadoFibonacci = new int [5];
 		int []resultadoFactorial = new int [5];
 		boolean []resultadoPrimos = new boolean [5];
-		int numFibonacci =0;
+		int numFibonacci = 0;
 		int n;
 		int v;
 		boolean x;
-		int numFactorial =0;
+		int numFactorial = 0;
 		
 		//Instanciar random
-		java.util.Random rm=new java.util.Random();
+		java.util.Random rm = new java.util.Random();
 		
+		//Rellenar array con numeros aleatorios
 		for (int i = 0; i < numeros.length; i++) 
 		{					
 			int num = rm.nextInt(1,10);
-			numeros[i]=num;
+			numeros[i] = num;
+		}
+		
+		//Rellenar array con numeros fibonacci
+		for (int i =0; i<numeros.length; i++) 
+		{
+			numFibonacci=numeros[i];
+			n = fibonacci(numFibonacci);
+			resultadoFibonacci[i] = n;
 		}
 		
 		
