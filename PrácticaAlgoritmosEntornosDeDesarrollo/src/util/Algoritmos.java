e6700dfd749267877759363884a25c1b0d3f8a6d
agl0041 <agl0041@alu.medac.es>
2024-04-19 18:55:37 +0200

    primer push empezando la clase abstracta Algoritmos

--- PrácticaAlgoritmosEntornosDeDesarrollo/src/util/Algoritmos.java
+++ PrácticaAlgoritmosEntornosDeDesarrollo/src/util/Algoritmos.java
@@ -1,5 +1,17 @@
+//ALVARO GARCIA LAMUDA
 package util;
 
-public class Algoritmos {
+public class Algoritmos 
+{
+	public static int factorial(int numero) 
+	{
+		int factorial = 1;
+		
+		for (int i = 1; i < numero; i++) 
+		{
+			factorial = factorial * i;
+		}
+		return factorial;
+	}
 
 }
