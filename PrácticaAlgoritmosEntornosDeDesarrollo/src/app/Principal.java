56213432f0d3499bfebfbce2219b398bf2cfbc81
agl0041 <agl0041@alu.medac.es>
2024-04-19 19:28:01 +0200

    añadiendo clase principal con main

--- PrácticaAlgoritmosEntornosDeDesarrollo/src/app/Principal.java
+++ PrácticaAlgoritmosEntornosDeDesarrollo/src/app/Principal.java
@@ -1,10 +1,33 @@
+//ALVARO GARCIA LAMUDA
 package app;
 
-public class Principal {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Principal 
+{
+	public static void main(String[] args) 
+	{
+		//Variables
+		int [] numeros = new int [5];
+		int []resultadoFibonacci = new int [5];
+		int []resultadoFactorial = new int [5];
+		boolean []resultadoPrimos = new boolean [5];
+		int numFibonacci =0;
+		int n;
+		int v;
+		boolean x;
+		int numFactorial =0;
+		
+		//Instanciar random
+		java.util.Random rm=new java.util.Random();
+		
+		for (int i = 0; i < numeros.length; i++) 
+		{					
+			int num = rm.nextInt(1,10);
+			numeros[i]=num;
+		}
+		
+		
+		
+		
 
 	}
-
 }
